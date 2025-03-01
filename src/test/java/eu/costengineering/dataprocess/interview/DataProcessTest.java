c18af7cc7ea000783e122465a77de932cbd5ecc2
Marin Jaic <mjaic3@gmail.com>
2025-03-01 19:58:07 +0100

    Files added

---
+++ src/test/java/eu/costengineering/dataprocess/interview/DataProcessTest.java
@@ -0,0 +1,32 @@
+package eu.costengineering.dataprocess.interview;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for {@link eu.costengineering.dataprocess.interview.CostEstimation}, feel free to add you own!
+ */
+public class DataProcessTest {
+
+    public static final double DELTA = 0.01;
+    public static Evaluator evaluator;
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        //fill in your Example CSV file's path
+        String pathName = "";
+        File createdFile = new File(pathName);
+        Parser exampleParser = null; // TODO: fill in your parser
+        evaluator = exampleParser.parseFile(createdFile);
+    }
+
+    @Test
+    public void exampleTest1() {
+        assertEquals(2100.0, evaluator.getDirectCost(), DELTA);
+    }
+}
